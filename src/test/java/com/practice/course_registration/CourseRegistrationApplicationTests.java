dc3a3c6970680444ef9e2597e7a7921ebec244a9
rud15dns <rud15dns@naver.com>
2025-10-22 19:16:29 +0900

    fix: CI 안되는 문제 >> - 일단 Kafka를 test에서 띄우지 않는 방향으로

--- src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
+++ src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
@@ -1,9 +1,12 @@
 package com.practice.course_registration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = KafkaAutoConfiguration.class)
 class CourseRegistrationApplicationTests {
 
 	@Test
