5e7ea65359600efae5856a82810386e5b25998a1
rud15dns <rud15dns@naver.com>
2025-10-22 19:48:42 +0900

    fix: kafkaProducer mock 객체 생성

--- src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
+++ src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
@@ -1,12 +1,23 @@
 package com.practice.course_registration;
 
+import com.practice.course_registration.global.kafka.KafkaProducer;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootTest(properties = "spring.kafka.enabled=false")
+@SpringBootTest
 class CourseRegistrationApplicationTests {
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public KafkaProducer kafkaProducer() {
+            // 실제 KafkaTemplate 등 의존을 가진 KafkaProducer를 Mockito로 대체
+            return Mockito.mock(KafkaProducer.class);
+        }
+        // 필요한 경우 KafkaTemplate 같은 다른 빈들도 여기서 mock으로 등록
+    }
 
 	@Test
 	void contextLoads() {
