9ae2c33e9ec98fd65445abc111a647c4e58aacae
rud15dns <rud15dns@naver.com>
2025-10-22 19:39:25 +0900

    fix: kafka관련 enable=false 설정

--- src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
+++ src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@ImportAutoConfiguration(exclude = KafkaAutoConfiguration.class)
+@SpringBootTest(properties = "spring.kafka.enabled=false")
 class CourseRegistrationApplicationTests {
 
 	@Test
