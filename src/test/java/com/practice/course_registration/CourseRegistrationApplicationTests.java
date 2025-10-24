38e7b07316e9d42ad18435f3728377ec3f4add4b
rud15dns <rud15dns@naver.com>
2025-10-24 21:14:41 +0900

    fix: EmbeddedKafka 사용 시도

--- src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
+++ src/test/java/com/practice/course_registration/CourseRegistrationApplicationTests.java
@@ -7,14 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092",
+        "port=9092"})
 class CourseRegistrationApplicationTests {
-    @MockitoBean
-    private KafkaProducer kafkaProducer;
 	@Test
 	void contextLoads() {
 	}
-
 }
