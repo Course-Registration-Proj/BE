6f7bdf35c95e347ae4c4cd04c622f2ac68745df4
rud15dns <rud15dns@naver.com>
2026-01-28 19:47:22 +0900

    test: 히카리 로깅 레벨 추가

--- src/main/java/com/practice/course_registration/domain/subject/service/SubjectService.java
+++ src/main/java/com/practice/course_registration/domain/subject/service/SubjectService.java
@@ -64,13 +64,13 @@ public class SubjectService {
         boolean held = false;
         try {
             // 멤버 찾기
-            //Member member = findMemberById(memberId);
+            Member member = findMemberById(memberId);
 
             // 해당 과목 찾기
-            //Subject subject = findByCode(code);
+            Subject subject = findByCode(code);
 
             // 유효성 검사
-            //validateCheck(member, subject);
+            validateCheck(member, subject);
 
             // 원자성 추가
             waitQueueService.enqueueGlobal(memberId, subject.getId(), System.currentTimeMillis());
